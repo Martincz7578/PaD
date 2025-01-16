3401e66c53e419294f456d1935f126240d6ac9a5
Martin <martinek@rychly.cz>
2025-01-16 08:47:26 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#home").click(function(){
+        window.location.href = "index.html";
+    });
+    $("#fw").click(function(){
+        window.location.href = "fw.html";
+    });
+    $("#os").click(function(){
+        window.location.href = "os.html";
+    });
+    $("#pds").click(function(){
+        window.location.href = "pds.html";
+    });
+    $("#l").click(function(){
+        window.location.href = "l.html";
+    });
+    $("#drivers").click(function(){
+        window.location.href = "drivers.html";
+    });
+    $("#1").click(function(){
+        window.location.href = "table.html";
+    });
+    $("#2").clicl(function(){
+        window.location.href = "form.html";
+    });
+});
